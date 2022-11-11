c64b8b57417f97fe73d41555b731b87cd747ceb0
Lovrevar <93528203+Lovrevar@users.noreply.github.com>
2022-11-11 11:36:38 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+using BlazorApp.Helpers;
+using BlazorApp.Services;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BlazorApp
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services
+                .AddScoped<IAccountService, AccountService>()
+                .AddScoped<IAlertService, AlertService>()
+                .AddScoped<IHttpService, HttpService>()
+                .AddScoped<ILocalStorageService, LocalStorageService>()
+                .AddScoped<IPostService, PostService>();
+
+            // configure http client
+            builder.Services.AddScoped(x => {
+                var apiUrl = new Uri(builder.Configuration["apiUrl"]);
+
+                // use fake backend if "fakeBackend" is "true" in appsettings.json
+                if (builder.Configuration["fakeBackend"] == "true")
+                {
+                    var fakeBackendHandler = new FakeBackendHandler(x.GetService<ILocalStorageService>());
+                    return new HttpClient(fakeBackendHandler) { BaseAddress = apiUrl };
+                 
+                }
+                
+                
+
+                return new HttpClient() { BaseAddress = apiUrl };
+            });
+            
+            var host = builder.Build();
+            
+            var postService = host.Services.GetRequiredService<IPostService>();
+            var accountService = host.Services.GetRequiredService<IAccountService>();
+            await postService.Initialize();
+            await accountService.Initialize();
+           
+
+            await host.RunAsync();
+        }
+    }
+}
